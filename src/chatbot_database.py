48a918064b2910610e6a6b3d5ad4449faedae066
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2017-12-03 18:54:20 -0500

    Added find_existing_score() and acceptable()

--- src/chatbot_database.py
+++ src/chatbot_database.py
@@ -30,13 +30,37 @@ def find_parent(pid):
         print('find_parent', e)
         return False
 
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print('find_existing_score', e)
+        return False
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data = '[deleted]' or data = '[removed]':
+        return False
+    else:
+        return True
+
 if __name__=="__main__":
     create_table()
     row_counter = 0
     paired_rows = 0
 
-    with open('/Users/lawnboymax/data/reddit_comments/{}/RC_{}'.format(timeframe.split('-')[0], timeframe), buffer=1000) as f:
+    with open('/Users/lawnboymax/data/reddit_comments/{}/RC_{}'.format(timeframe.split('-')[0], timeframe), buffering=1000) as f:
         for row in f:
+            print(row)
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
@@ -46,4 +70,11 @@ if __name__=="__main__":
             subreddit = row['subreddit']
 
             parent_data = find_parent(parent_id)
+
+            if score >= 2: #threshold to filter useless comments (low score)
+                existing_comment_score = find_existing_score(parent_id)
+                if existing_comment_score:
+                    if score > existing_comment_score:
+
+
 
