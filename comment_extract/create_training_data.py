26d493b5ebad33afc70da9447f2621e888692eec
Maxim Mikhaylov <lawnboymadmax@gmail.com>
2017-12-21 18:48:19 -0500

    Added multiprocessing

--- comment_extract/create_training_data.py
+++ comment_extract/create_training_data.py
@@ -2,12 +2,8 @@ import sqlite3
 import pandas as pd
 import os
 
-timeframes = ['2017-11']
-data_dir = os.path.abspath('/Users/lawnboymax/data/reddit_comments/')
-train_dir = os.path.join(data_dir, 'model_data', 'train')
-test_dir = os.path.join(data_dir, 'model_data', 'test')
+def create_training_data(timeframe)
 
-for timeframe in timeframes:
     connection = sqlite3.connect(os.path.join(data_dir, 'dbs', '{}.db'.format(timeframe)))
     c = connection.cursor()
     limit = 5000
@@ -30,7 +26,7 @@ for timeframe in timeframes:
                     f.write(content + '\n')
             test_done = True
         else:
-	"""
+        """
         with open(os.path.join(train_dir, '[{}]train.from'.format(timeframe)), 'a', encoding='utf8') as f:
             for content in df['parent'].values:
                 f.write(content + '\n')
@@ -41,3 +37,15 @@ for timeframe in timeframes:
         counter += 1
         if counter % 20 == 0:
             print(counter*limit, 'rows completed')
+            
+data_dir = os.path.abspath('/Volumes/glyph_500/data/reddit_comments')
+train_dir = os.path.join(data_dir, 'model_data', 'train')
+test_dir = os.path.join(data_dir, 'model_data', 'test')
+timeframes = ['2017-01', '2017-02', '2017-03', '2017-04', '2017-05', '2017-06', '2017-07', '2017-08', '2017-09', '2017-10', '2017-11', '2016-12', '2016-11', '2016-10', '2016-09', '2016-08']
+
+if __name__ == '__main__':
+    startTime = datetime.now()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for timeframe,_ in zip(timeframes,executor.map(create_training_data,timeframes)):
+            pass    
+    print(datetime.now() - startTime)
